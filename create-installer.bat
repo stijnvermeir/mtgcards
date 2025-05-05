bf8ba164efb5df1125bb55a2c484288cef11efdc
Stijn Vemeir <vermeir.stijn@gmail.com>
2025-05-05 20:48:47 +0200

    Update deploy script

--- create-installer.bat
+++ create-installer.bat
@@ -2,8 +2,8 @@ cd %~dp0
 rmdir /S /Q deploy
 mkdir deploy
 cd deploy
-copy ..\build-release\release\MTGCards.exe .
-set PATH=%SystemDrive%\Qt\5.15.0\msvc2019\bin\
+copy ..\mtgcards\build\Desktop_Qt_6_9_0_MSVC2022_64bit-Release\release\MTGCards.exe .
+set PATH=%SystemDrive%\Qt\6.9.0\msvc2022_64\bin
 windeployqt --verbose 8 MTGCards.exe
-"%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" ..\setup.iss
+REM "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" ..\setup.iss
 pause
